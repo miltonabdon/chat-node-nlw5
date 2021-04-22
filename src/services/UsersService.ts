1ce488a985b7e3e9c22e394dd8544902e94d50df
Milton <miltonabdon69@gmail.com>
2021-04-22 20:18:37 -0300

    novos controllers, services e repositories

---
+++ src/services/UsersService.ts
@@ -0,0 +1,30 @@
+import { getCustomRepository, Repository } from "typeorm";
+import { User } from "../entities/User";
+import { UsersRepository } from "../repositories/UsersRepository";
+class UsersService {
+  private userRepository: Repository<User>;
+
+  constructor() {
+    this.userRepository = getCustomRepository(UsersRepository);
+  }
+
+  async create(email: string) {
+    const userExists = await this.userRepository.findOne({
+      email,
+    });
+
+    if (userExists) {
+      return userExists;
+    }
+
+    const user = this.userRepository.create({
+      email,
+    });
+
+    await this.userRepository.save(user);
+
+    return user;
+  }
+}
+
+export { UsersService };
